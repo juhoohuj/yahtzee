9a4d64510681aacfbb1badc6c628b234fd8db292
Juho Ahonen <juho.ahonen2@gmail.com>
2023-03-09 00:54:09 +0200

    dicethrow function

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,13 +1,40 @@
 import { View, Text, } from "react-native"
-import { FontAwesome5 } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Home, NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BONUS_POINTS, NBR_OF_SCOREBOARD_ROWS, PLAYER} from "./Home";   
 import { useState } from "react";
 
-const Gameboard = () => {
+let icons = ["dice-1", "dice-2", "dice-3", "dice-4", "dice-5", "dice-6"]
+
+function randomNum() {
+   return Math.floor(Math.random() * 6) + 1;
+}
+
+let dices = [0,0,0,0,0,0]
+
+function diceThrow() {
+    for (let i = 0; i < dices.length; i++) {
+        dices[i] = randomNum()
+    }
+
+    console.log(dices)
+    
     return(
-        <Text>{PLAYER}</Text>
+        dices.map(n => 
+        <MaterialCommunityIcons name={icons[n - 1]} size={50} color="black" />  
+        )
     )
 }
 
+let dicesThrown = []
+
+
+const Gameboard = () => {
+
+    return(
+        <Text>{diceThrow()}</Text>
+    )
+}
+
+diceThrow()
 
 export {Gameboard};
