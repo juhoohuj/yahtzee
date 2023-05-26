3d3c5ada7bfc00289ec103f33cd2a4ac91b03da5
Juho Ahonen <juho.ahonen2@gmail.com>
2023-05-26 15:23:53 +0300

    game ready

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -2,15 +2,29 @@ import { View, Text, ViewComponent, Button, Pressable} from "react-native"
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Home, NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BONUS_POINTS, NBR_OF_SCOREBOARD_ROWS, PLAYER} from "./Home";   
 import { useState, useEffect } from "react";
+import Styles from "../styles/Styles.js";
+
 
 const icons = ["dice-1", "dice-2", "dice-3", "dice-4", "dice-5", "dice-6"]
 
-function randomNum() {
-   return Math.floor(Math.random() * 6) + 1;
-}
-
 
 const Gameboard = () => {
+
+useEffect(() => {
+  let totalScore = 0;
+
+  points.forEach((item) => {
+    totalScore += item[1];
+  });
+  setScore(totalScore);
+}, [addToScore])
+
+
+
+  function randomNum() {
+    return Math.floor(Math.random() * 6) + 1;
+ }
+
     const [points, setPoints] = useState([
       [1, 0, true],
       [2, 0, true],
@@ -20,9 +34,11 @@ const Gameboard = () => {
       [6, 0, true]
     ]);
   
-    let dices = [[0, false], [0, false], [0, false], [0, false], [0, false]];
+    const [dices, setDices] = useState([[0, false], [0, false], [0, false], [0, false], [0, false]])
     const [thrownDices, setThrownDices] = useState([]);
     const [throwsLeft, setThrowsLeft] = useState(NBR_OF_THROWS);
+    const [spotsLeft, setSpotsLeft] = useState(6)
+    const [score, setScore] = useState(0);
 
     function checkPoints() {
       let pointsLeft = false;
@@ -35,19 +51,20 @@ const Gameboard = () => {
     }
   
     function diceThrow() {
-
-
-      if(checkPoints() === true) {
+      if(checkPoints() === true ) {
 
         for (let i = 0; i < dices.length; i++) {
           if (dices[i][1] === false) {
             dices[i][0] = randomNum();
           }
+
+          else {
+            dices[i][0] = dices[i][0];
+          }
         }
 
         setThrownDices([...dices]);
         setThrowsLeft(throwsLeft - 1);
-        console.log(throwsLeft);
       } else {
         alert("No points left to add");
       }
@@ -57,10 +74,11 @@ const Gameboard = () => {
         return (
           <View style={{ flexDirection: 'row', alignSelf: "center" }}>
             {thrownDices.map((n, index) => {
+
               const diceNumber = n[0];
               const isAddedToPoints = !points[diceNumber - 1][2];
               const isSelected = n[1];
-      
+
               let color = "orange";
               if (isSelected) {
                 color = "green";
@@ -88,29 +106,36 @@ const Gameboard = () => {
       
         if (!isAlreadyAdded) {
           item[1] = !item[1];
-          setThrownDices([...thrownDices]); // Update the thrownDices state
+          setThrownDices([...thrownDices]);
         }
 
         console.log(thrownDices);
       }
   
       function addToScore() {
-        thrownDices.forEach((item) => {
-          if (item[1] === true) {
-            let number = item[0];
-            points[number - 1][1] += number;
-            points[number - 1][2] = false;
-          }
-        });
-        console.log(points);
-        setThrownDices([]);
+        if (throwsLeft == 0) {
+          thrownDices.forEach((item) => {
+            if (item[1] === true) {
+              let number = item[0];
+              points[number - 1][1] += number;
+              points[number - 1][2] = false;
+            }
+          });
+          console.log(points);
+          setThrownDices([]);
+          setThrowsLeft(NBR_OF_THROWS);
+          setDices([[0, false], [0, false], [0, false], [0, false], [0, false]]);
+        }
+
+        else {
+          alert("You need to throw the dices first");
+        }
       }
 
     return(
-        <View>
-            <Text>{thrownDices}</Text>
+        <View style={Styles.container}>
             <Buttons/>
-            <Button onPress={diceThrow} title="Throw dices" />
+            {throwsLeft > 0 ? <Button onPress={diceThrow} title="Throw dices" /> : null}
             <Button onPress={addToScore} title="Add to score" />
             <Text>Throws left: {throwsLeft}</Text>
             <Text>POINTS</Text>
@@ -124,11 +149,13 @@ const Gameboard = () => {
                     )
                 })}
             </View>
-                <Button title="test" onPress={checkPoints}/>
+
+                <Text>Current score: {score}</Text>
         </View>
 
     )
 }
+
 
 
 export {Gameboard};
